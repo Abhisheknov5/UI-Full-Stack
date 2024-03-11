3b34cac5ec166ba775506bf288f2ade3bd16eba5
Abhinov5 <abhishek.kumar@speqto.com>
2024-03-11 05:56:22 +0530

    Ui-Fullstack

---
+++ AJAX/shopper-site-Project/scripts/index.js
@@ -0,0 +1,15 @@
+function LoadPage(page){
+    var http = new XMLHttpRequest();
+    http.open("get",page, true);
+    http.send();
+    http.onreadystatechange = function(){
+        if(http.readyState==4){
+            console.log(http.responseText);
+            document.querySelector("section").innerHTML = http.responseText;
+        }
+    }
+}
+
+function bodyload(){
+    LoadPage("home.html");
+}
